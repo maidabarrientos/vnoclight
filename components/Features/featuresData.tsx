69396ab3a8b89be64fbada9476b8d068f16a79a0
GlobalVentures.com <maidabarrientos@gmail.com>
2023-08-26 10:42:26 +0800

    first commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,90 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/015-branding.svg",
+    title: "Nurture Your Brand",
+    description:
+      "Effortlessly manage your digital assets and foster secure collaboration among your teams and contributors using VNOC's advanced Brand Management tools.",
+  },
+  {
+    id: 2,
+    icon: "/images/icon/034-analysis.svg",
+    title: "Monetize with Flexibility",
+    description:
+      "Unlock your brand's potential through flexible domain monetization features that optimize referrals and network ad optimization, providing a tailored approach to revenue generation.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/040-coordination.svg",
+    title: "Find Talent, Build Brilliance",
+    description:
+      "Effortlessly find, hire, and collaborate with talented individuals within the Contrib network, fueling your brand's growth and success.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/007-content strategy.svg",
+    title: "Companies: Simplify Management",
+    description:
+      "Create and manage companies seamlessly with VNOC's single-panel interface, streamlining portfolio oversight and empowering efficient management.",
+  },
+  {
+    id: 5,
+    icon: "/images/icon/023-bitcoin.svg",
+    title: "Blockchain Empowerment",
+    description:
+      "Harness the power of blockchain by pushing your digital assets through our eShares protocol. Offer fractional ownership and smart equity, all while efficiently managing contributions via Contrib.",
+  },
+  {
+    id: 6,
+    icon: "/images/icon/013-communications.svg",
+    title: "Streamlined Communication",
+    description:
+      "VNOC offers versatile communication tools that integrate seamlessly with your existing workflow, enabling dynamic engagement with visitors and contributors.",
+  },
+  {
+    id: 7,
+    icon: "/images/icon/026-graphene.svg",
+    title: "Master Vertical Management",
+    description:
+      "Turn visitors into engaged contributors with VNOC's proven frameworks, meticulously designed to drive interaction and growth within your brand.",
+  },
+  {
+    id: 8,
+    icon: "/images/icon/021-multitask.svg",
+    title: "Unleash Network Effects",
+    description:
+      "VNOC's integrated People Network and customized apps create dynamic network effects within your asset portfolio, ensuring efficient management of leads, contributors, and team members, all from one intuitive panel.",
+  },
+  {
+    id: 9,
+    icon: "/images/icon/017-influencer.svg",
+    title: "Social Impact, Simplified",
+    description:
+      "Experience the ease of VNOC's Social Suite application, streamlining content creation, management, and distribution across all social platforms with a single action.",
+  },
+  {
+    id: 10,
+    icon: "/images/icon/icon-06.svg",
+    title: "Contrib Network",
+    description:
+      "Utilize the Contrib Network to allow individuals to contribute value to your venture via internal contrib apps. Connect with professionals, optimize workflow, and drive collaboration.",
+  },
+  {
+    id: 11,
+    icon: "/images/icon/006-ambassador.svg",
+    title: "Blockchain Management",
+    description:
+      "Empower your ventures with RealtyDAO, our integrated tokenization platform. Transform real estate and assets into digital tokens on the blockchain. Offer fractional ownership (smart equity) while managing contributions via Contrib.",
+  },
+  {
+    id: 12,
+    icon: "/images/icon/019-bionic eye.svg",
+    title: "Flexible Stack",
+    description:
+      "Bring your own stack or use ours. VNOC is flexible to enable localized growth between portfolios.",
+  },
+];
+
+export default featuresData;
