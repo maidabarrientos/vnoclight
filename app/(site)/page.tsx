9917ecd490989769f09e163881733bedb46881fe
Andre Cipriano <andreboncipriano@gmail.com>
2023-09-20 21:18:42 +0800

    andre

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,6 +8,7 @@ import FunFact from "@/components/FunFact";
 import Integration from "@/components/Integration";
 import CTA from "@/components/CTA";
 import FAQ from "@/components/FAQ";
+import Pricing from "@/components/Pricing";
 import Contact from "@/components/Contact";
 import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
@@ -32,6 +33,7 @@ export default function Home() {
       <CTA />
       <FAQ />
       <Testimonial />
+      <Pricing />
       <Contact />
       <Blog />
     </main>
